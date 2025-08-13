6a59c4666791ffc385578d03ee17995fe8f8513f
Алексей Головизин <agolovizin@alfabank.ru>
2025-08-13 20:42:17 +0300

    app.py: возвращаем как было. И сохраняем "наработки", хватит этого ада на сегодня...

--- task_1/mongo_3/run_with_minikube_tunnel.sh
+++
@@ -1,5 +0,0 @@
-# Проверим каждую ноду
-for port in 30001 30002 30003 30004 30005 30006; do
-  echo "Checking port $port:"
-  redis-cli -h minikube-docker-p $port -a redis-dev-password ping 2>/dev/null || echo "  Failed"
-done
